afe2ccce150ef945b6e40dde59a726b6eb56cb07
RJ Hallsted <rhallste@student.42.us.org>
2017-11-18 14:43:28 -0800

    passing tests, just need to norm and fix makefile

--- fillit.h
+++ fillit.h
@@ -6,7 +6,7 @@
 /*   By: rhallste <rhallste@student.42.us.org>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/27 14:35:15 by rhallste          #+#    #+#             */
-/*   Updated: 2017/11/06 18:46:39 by rhallste         ###   ########.fr       */
+/*   Updated: 2017/11/18 14:42:33 by rhallste         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 
 # include <string.h>
 
-# define ERROR_RETURN(i) { ft_putendl("Error"); return (i); }
+# define ERROR_RETURN(i) { ft_putendl("error"); return (i); }
 # define BUFF_SIZE 545
 
 enum
